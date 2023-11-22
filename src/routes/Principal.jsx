6a5b0c49ddcb0f668332eb24db2f9932c20be1b0
MachadONLY <rm551570@fiap.com.br>
2023-11-22 19:51:03 -0300

    .

--- src/routes/Principal.jsx
+++ src/routes/Principal.jsx
@@ -4,6 +4,7 @@ import relogio from '../assets/img/time.png'
 import video from '../assets/img/video.png'
 import conversa from '../assets/img/talk.png'
 
+
 //página de apresentação da marca
 export default function Principal() {
   return (
@@ -11,24 +12,24 @@ export default function Principal() {
     <main>
         <section className="slideshow">
             <img src={mainimg} alt="Imagem principal de agendamento" />
-            <div className="c-apresentacao-texto">
+            <div className="slideshow-text">
                 <h2>HAP +</h2>
-                <p>Aplicativo simples e intuitivo que busca facilitar a processo de solicitação de exames, simplificando a comunicação entre os usuários e os locais de exame, tanto conveniados quanto clínicas particulares.</p>
+                <p>Desenvolver uma plataforma de telemedicina para a Hapvida, visando facilitar o acesso a consultas médicas, melhorar o gerenciamento de medicamentos, promover a gestão eficiente de vacinas e proporcionar uma experiência abrangente de saúde para os pacientes.</p>
             </div>
         </section>
         <section className="c-solucao">
             <div>
-                <h3>O QUE É A SOLUÇÃO</h3>
-                <p>Nossa solução é um aplicativo que será focado em facilitar para o usuário a solicitação de exames, simplificando a comunicação entre o convênio e locais de exame, tanto conveniados como clínicas particulares. O objetivo desta é garantir que os usuários tenham acesso facilitado aos procedimentos médicos necessários para uma vida mais saudável. 
+                <h3>SOLUÇÃO</h3>
+                <p>A plataforma de telemedicina desenvolvida apresenta uma solução inovadora e abrangente para otimizar a experiência dos usuários no cuidado com a saúde. Integrando agendamento de consultas, consultas virtuais, gerenciamento de medicamentos e vacinas, além de notificações personalizadas, a plataforma visa simplificar e aprimorar a jornada do paciente. Com um foco especial na eficiência, a solução oferece um check-in rápido no hospital, permitindo que os usuários agendem suas consultas online e realizem um check-in presencial de forma ágil, usando a tecnologia NFC. Essa abordagem inovadora redefine a interação entre paciente e sistema de saúde, proporcionando conveniência, eficácia e um cuidado mais personalizado. 
                 </p>
             </div>
             <div>
-                <h3>O QUE ELA FARÁ</h3>
-                <p>Nossa solução vai permitir que o usuário, a partir de uma solicitação médica, consiga de forma simples e rápida agendar exames com nossas clínicas parceiras, de acordo com a data, horário e local desejados. A aplicação, vinculada a um plano de saúde, possibilitará que os usuário e profissionais visualizem seus resultados, através do uso de sensores e Internet das Coisas, para garantir mais automação. </p>
+                <h3>UTILIDADE</h3>
+                <p>A plataforma de telemedicina oferece uma gama abrangente de funcionalidades projetadas para simplificar a gestão da saúde do usuário. Ao agendar consultas de forma intuitiva, participar de consultas virtuais, gerenciar medicamentos e registrar vacinas, os usuários têm acesso a uma experiência de cuidado integrada e eficiente. Além disso, a plataforma emite notificações automáticas para lembretes de consultas, horários de medicamentos e atualizações sobre vacinas. A solução também revoluciona o processo de check-in no hospital, permitindo que os usuários cheguem e ingressem na sala de espera com facilidade, utilizando a tecnologia NFC para uma interação rápida e sem complicações. </p>
             </div>
             <div>
-                <h3>COMO FUNCIONARÁ</h3>
-                <p>Ao solicitar o agendamento, o usuário terá acesso aos locais disponíveis em um mapa, aos horários e datas oferecidos e o preço da consulta (caso seja fora do convênio). Os resultados dos exames estarão disponíveis para consulta dos usuários e dos médicos. Para garantir o uso da tecnologia para automação, caso o paciente precise ficar em observação ou tratamento contínuo, a partir de sensores disponibilizados, informações médicas como temperatura/febre e sinais de movimento estarão disponíveis remotamente no aplicativo, através de IOT.</p>
+                <h3>FUNCIONALIDADE</h3>
+                <p>A plataforma de telemedicina oferece um conjunto robusto de funcionalidades para otimizar a gestão da saúde dos usuários. Isso inclui um sistema intuitivo de agendamento de consultas que permite aos usuários escolherem horários convenientes, consultas virtuais para um acesso rápido e seguro aos profissionais de saúde, e um eficiente gerenciamento de medicamentos, com lembretes precisos para garantir a adesão ao tratamento. A funcionalidade de registro de vacinas fornece aos usuários um histórico claro e notificações automáticas para garantir que estejam em dia com as imunizações recomendadas. Além disso, a inovação no check-in hospitalar, através da tecnologia NFC, agiliza o processo, economizando tempo e proporcionando uma transição suave do agendamento online para a sala de espera do médico. Essas funcionalidades combinadas visam melhorar significativamente a experiência do paciente e promover uma abordagem proativa para o cuidado com a saúde.</p>
             </div>
         </section>
         <section className="c-vantagens">
