bf2cc1333337510c97395802e7bfe2766e5c40b8
MachadONLY <rm551570@fiap.com.br>
2023-11-23 10:35:14 -0300

    mudei aq

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,22 @@
-import '../assets/styles/Rodape.css'
+import '../assets/styles/Rodape.css';
 
 export default function Rodape() {
   return (
-      <>
-        <footer>
-          <p>Av. Paulista, 1106 - 4º andar - São Paulo - SP</p>
-          <p>Gabriel Teixeira Machado - RM551570</p>
-          <p>João Lucas Yudi Redi Handa - RM98458</p>
-          <p>ESPG</p>
-        </footer>
-      </>
+    <>
+      <footer>
+        <div className="footer-content">
+          <div className="footer-address">
+            <p>Av. Paulista, 1106 - 4º andar - São Paulo - SP</p>
+          </div>
+          <div className="footer-team">
+            <p>Gabriel Teixeira Machado - RM551570</p>
+            <p>João Lucas Yudi Redi Handa - RM98458</p>
+          </div>
+          <div className="footer-class">
+            <p>ESPG</p>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
